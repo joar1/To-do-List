2719d0827ee4b500d71d81b8e6c6060e7158910c
Joar <joar1.kodehode@gmail.com>
2022-05-20 10:22:07 +0200

    .

---
+++ index.js
@@ -0,0 +1,48 @@
+//Things to the DOM
+const toDoInput = document.querySelector('input')
+const toDoList = document.querySelector('#toDoList')
+const addToDoBtn = document.querySelector('#toDoBtn')
+const deleteAllBtn = document.querySelector('#deleteAllBtn')
+
+
+//Date
+let currentTime = new Date();
+
+// The input and a clear command.
+toDoInput.addEventListener('keydown', (e) =>{
+   if (e.key !== 'Enter' || toDoInput.value.match(/^ *$/)) return
+   console.log("Adding ", toDoInput.value.toUpperCase(), " to the list")
+   addToDo()
+   toDoInput.value = ""
+})
+
+
+//The add-to-do button which adds to the list and clears the input.
+addToDoBtn.addEventListener('click', () =>{
+   if(toDoInput.value.match(/^ *$/))return
+   console.log("Adding ", toDoInput.value.toUpperCase(), " to the list", currentTime)
+   addToDo()
+   toDoInput.value = ""
+})
+
+
+
+//Finding and deleting each list elements.
+deleteAllBtn.addEventListener('click', () =>{
+   const deleteThemAll = document.querySelectorAll('li')
+   deleteThemAll.forEach(toDo => toDo.remove())   
+})
+
+
+
+// Where to magic happens, adds a list, and a delete button.
+function addToDo(){
+   const toDo = document.createElement('li')
+   const toDoText = document.createElement('p')
+   const deleteBtn = document.createElement('button')
+   toDoText.textContent = toDoInput.value
+   deleteBtn.textContent = "DELETE"
+   deleteBtn.addEventListener('click', () => toDo.remove())
+   toDoList.append(toDo)
+   toDo.append(toDoText, deleteBtn)
+}
