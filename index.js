87c64fb683fab0a9c8e28ef4bda0a1bed29b4eba
Joar <joar1.kodehode@gmail.com>
2022-05-25 14:33:35 +0200

    .

--- index.js
+++ index.js
@@ -5,19 +5,17 @@ const addToDoBtn = document.querySelector('#toDoBtn')
 const deleteAllBtn = document.querySelector('#deleteAllBtn')
 
 
-//Date
-let currentTime = new Date();
-
-// The input and a clear command.
+// The input and a clear command with keydown.
 toDoInput.addEventListener('keydown', (e) =>{
    if (e.key !== 'Enter' || toDoInput.value.match(/^ *$/)) return
    console.log("Adding ", toDoInput.value.toUpperCase(), " to the list")
    addToDo()
    toDoInput.value = ""
+   toDoInput.focus()
 })
 
 
-//The add-to-do button which adds to the list and clears the input.
+//The add-to-do button, which adds to the list and clears the input. The same as above, just with click.
 addToDoBtn.addEventListener('click', () =>{
    if(toDoInput.value.match(/^ *$/))return
    console.log("Adding ", toDoInput.value.toUpperCase(), " to the list")
@@ -27,7 +25,6 @@ addToDoBtn.addEventListener('click', () =>{
 })
 
 
-
 //Finding and deleting each list elements.
 deleteAllBtn.addEventListener('click', () =>{
    const deleteThemAll = document.querySelectorAll('li')
@@ -35,8 +32,7 @@ deleteAllBtn.addEventListener('click', () =>{
 })
 
 
-
-// Where to magic happens, adds a list, and a delete button and class to style them in css.
+// Where the magic happens, creates a list, delete button and class to style them in css.
 function addToDo(){
    const toDo = document.createElement('li')
    const toDoText = document.createElement('p')
@@ -45,6 +41,6 @@ function addToDo(){
    deleteBtn.textContent = "Delete"
    deleteBtn.addEventListener('click', () => toDo.remove())
    deleteBtn.classList.add('deleteBtn', 'p')
-   toDoList.append(toDo)
+   toDoList.prepend(toDo)
    toDo.append(toDoText, deleteBtn)
 }
